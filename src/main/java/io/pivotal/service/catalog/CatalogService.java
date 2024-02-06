2f3e4a7f4da6da68e1c1108a6e4ba4f377e6b7f0
Keith Kester <keith.a.kester.ctr@army.mil>
2024-02-06 13:45:46 -0600

    Schema fix

--- src/main/java/io/pivotal/service/catalog/CatalogService.java
+++ src/main/java/io/pivotal/service/catalog/CatalogService.java
@@ -1,16 +1,11 @@
 package io.pivotal.service.catalog;
 
 import io.pivotal.service.product.Product;
-import io.pivotal.service.product.ProductEntity;
-import io.pivotal.service.product.ProductMapper;
 import io.pivotal.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
