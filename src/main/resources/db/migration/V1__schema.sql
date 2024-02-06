bb749bfa6109888b506b286249a526906317c9d0
Keith Kester <keith.a.kester.ctr@army.mil>
2024-02-06 14:08:38 -0600

    Refined Schema

--- src/main/resources/db/migration/V1__schema.sql
+++ src/main/resources/db/migration/V1__schema.sql
@@ -1,5 +1,20 @@
-create table catalogs (id varchar(255) not null, code varchar(255) not null, display_name varchar(255), primary key (id));
+create table catalogs (
+    id uuid not null,
+    code varchar(255) not null,
+    display_name varchar(255),
+    primary key (id)
+);
 alter table catalogs add constraint code_unique unique(code);
-create table products (id uuid not null, sku varchar(255) not null, description varchar(255), name varchar(255), price varchar(255), catalog_id varchar(255), quantity integer, primary key (id));
+
+create table products (
+    id uuid not null,
+    sku varchar(255) not null,
+    description varchar(255),
+    name varchar(255),
+    price varchar(255),
+    catalog_id varchar(255),
+    quantity integer,
+    primary key (id)
+);
 alter table products add constraint FKr9g72vsfwc9lupwutnut4w7kf foreign key (catalog_id) references catalogs;
 alter table products add constraint catalog_sku_unique unique(catalog_id,sku);
