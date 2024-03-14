ed3d23fb348f91dfda99881e33bd83cf23e42943
Keith Kester <keith.a.kester.ctr@army.mil>
2024-03-14 15:28:51 -0500

    imporved tests

--- src/main/resources/db/migration/V1__schema.sql
+++ src/main/resources/db/migration/V1__schema.sql
@@ -10,10 +10,10 @@ create table products (
     id uuid not null,
     sku varchar(255) not null,
     description varchar(255),
-    name varchar(255),
-    price varchar(255),
-    catalog_id varchar(255),
-    quantity integer,
+    name varchar(255) not null,
+    price varchar(255) not null,
+    catalog_id varchar(255) not null,
+    quantity integer not null,
     primary key (id)
 );
 alter table products add constraint FKr9g72vsfwc9lupwutnut4w7kf foreign key (catalog_id) references catalogs;
